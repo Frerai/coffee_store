613d44a719bf1f9a6193f0590d22e468df26556f
Feraidoon Hamidi <feraidoon321@hotmail.com>
2024-07-14 15:00:11 +0200

    fix: Add temporary endpoints for drinks and admin

--- app/main.py
+++ app/main.py
@@ -6,3 +6,21 @@ app = FastAPI()
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message": "Hello World!"}
+
+
+# Temporary endpoint for beverages.
+@app.get("/drinks")
+async def get_drinks() -> list[str]:
+    return ["Black Coffee", "Latte", "Mocha", "Tea"]
+
+
+# Temporary endpoint for toppings.
+@app.get("/drinks/toppings")
+async def get_toppings() -> list[str]:
+    return ["Milk", "Hazelnut syrup", "Chocolate sauce", "Lemon"]
+
+
+# Temporary endpoint for admin creating drinks.
+@app.get("/admin/create")
+async def create_drink() -> str:
+    return "Created."
